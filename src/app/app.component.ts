1dfa7ec36a54a671952adce42d009c223e360042
shigueojp <okuhamail@gmail.com>
2020-03-12 17:44:43 -0300

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `
+  <nav-bar></nav-bar>
+  <events-list></events-list>
+  `
+})
+export class AppComponent {
+  title = 'ng-intro';
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav/navbar.component';
+import { EventsListComponent } from './events/events-list.component';
+import { EventThumbnailComponent } from './events/event-thumbnail.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavBarComponent,
+    EventsListComponent,
+    EventThumbnailComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
