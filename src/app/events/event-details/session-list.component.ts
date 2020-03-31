2664e635378602c1cd608c47c02486625d878928
shigueojp <okuhamail@gmail.com>
2020-03-30 23:41:15 -0300

    Adding Session List Component

---
+++ src/app/events/event-details/session-list.component.ts
@@ -0,0 +1,30 @@
+import { Component, Input, OnChanges } from "@angular/core";
+import { ISession } from '../shared/events.model';
+
+@Component({
+    selector: 'session-list',
+    templateUrl: './session-list.component.html'
+})
+
+export class SessionListComponent implements OnChanges {
+    @Input() sessions: ISession[]
+    @Input() filterBy: string
+    visibleSessions: ISession[] = [];
+
+    ngOnChanges() {
+        if (this.sessions) {
+            this.filterSessions(this.filterBy)
+        }
+    }
+
+    filterSessions(filter) {
+        if (filter === 'all') {
+            // Need to slice because is object and need to be cloned
+            this.visibleSessions = this.sessions.slice(0);
+        } else {
+            this.visibleSessions = this.sessions.filter(session => {
+                return session.level.toLocaleLowerCase() === filter;
+            });
+        }
+    }
+}
